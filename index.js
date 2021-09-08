a3d949714335d8ed3760ba4e9b205b275acb412b
ashes-S <65926031+ashes-S@users.noreply.github.com>
2021-09-08 22:54:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+// document.querySelectorAll("button")[0].style.backgroundColor = "black";
+// document.querySelector("input").placeholder="F U GLOBU";
+// document.querySelector("footer h4").innerHTML="@2021 DOG PEE";
